040688a970e57e66a6cd705c1682b3f0e21772c6
MihaLis333 <m190333@ya.ru>
2023-02-27 13:31:14 +0300

    initial commit

---
+++ Seminar1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+double a = 9;
+double b = 3;
+
+if (a * a == b)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+} */
+
+
+/* Console.WriteLine("Введите число");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Квадратa числа a = {a * a}"); */
+
+/*
+Console.WriteLine("Enter the number: ");
+int N = Convert.ToInt32 (Console.ReadLine());
+
+int count = -N;
+
+while (count <= N)
+{
+    Console.Write(count);
+    count++;
+} */
+
